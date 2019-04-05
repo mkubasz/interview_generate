61e9d1f9d43406f59f7b4e64ead307cfb5431bb0
mkubasz <mkubasz@gmail.com>
2019-04-05 15:50:39 +0200

    Add routes for methods

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,17 @@
 
 #[macro_use]
 extern crate rocket;
+extern crate uuid;
 
-use std::collections;
+use std::fs;
 use std::result;
 use std::hash::Hash;
+use std::collections;
+use uuid::Uuid;
 use rocket::response::content::Json;
-
 
 enum Tag {
     OOP
-}
-
-impl Tag {
-    fn to_str(&self) -> &'static str {
-        match self {
-            Tag::OOP => return "oop",
-        }
-    }
 }
 
 enum Level {
@@ -27,18 +21,8 @@ enum Level {
     SENIOR,
 }
 
-impl Level {
-    fn to_str(&self) -> &'static str {
-        match self {
-            Level::JUNIOR => return "Junior",
-            Level::MEDIUM => return "Medium",
-            Level::SENIOR => return "Senior"
-        }
-    }
-}
-
 struct Question {
-    id: i32,
+    id: Option<Uuid>,
     question: String,
     answer: String,
     level: Level,
@@ -46,42 +30,94 @@ struct Question {
 }
 
 struct Filter {
-    number: i32,
+    number: u16,
     level: Level,
     tags: Vec<Tag>,
 }
 
+struct QuestionRepo;
 
-trait QuestionRepo {
+impl QuestionRepo {
     fn create(question: Question) -> Result<(), ()> {
         Ok(())
     }
 
-    fn get(filter: Filter) -> Option<Vec<Question>> {
-        None
+    fn get() -> Option<Vec<&'static str>> {
+        Some(vec!["d"])
     }
 
     fn update(question: Question) -> Result<(), ()> {
         Ok(())
     }
 
-    fn delete(id: i32) -> Result<(), ()> {
+    fn delete(id: Uuid) -> Result<(), ()> {
         Ok(())
     }
 }
 
 #[get("/")]
 fn index() -> Json<&'static str> {
-}
-
-#[get("/get")]
-fn get() -> Json<&'static str> {
     Json("{'routes' : ['/get/<topic>/<level>/<tag>']}")
 }
 
+#[get("/random?<number>&<level>&<topic>")]
+fn random(number: u16, level: String, topic: Option<String>) -> Json<&'static str> {
+    let st = format!("{{'routes' : ['/random/<{}>/<{}>/<{:?}>']}}", number, level, topic);
+    Json(Box::leak(st.into_boxed_str()))
+}
+
+mod question {
+    use rocket_contrib::json::Json;
+
+    #[get("/question")]
+    pub fn read() -> Json<&'static str> {
+        Json("{'routes' : ['/get/<topic>/<level>/<tag>']}")
+    }
+
+    #[post("/question")]
+    pub fn create() -> Json<&'static str> {
+        Json("{'routes' : ['/get/<topic>/<level>/<tag>']}")
+    }
+
+    #[put("/question")]
+    pub fn update() {}
+
+    #[delete("/question")]
+    pub fn delete() {}
+}
+
+mod tag {
+    use rocket_contrib::json::Json;
+
+    #[get("/tag")]
+    pub fn read() -> Json<&'static str> {
+        Json("{'routes' : ['/get/<topic>/<level>/<tag>']}")
+    }
+
+    #[post("/tag")]
+    pub fn create() -> Json<&'static str> {
+        Json("{'routes' : ['/get/<topic>/<level>/<tag>']}")
+    }
+
+    #[put("/tag")]
+    pub fn update() {}
+
+    #[delete("/tag")]
+    pub fn delete() {}
+}
+
 fn main() {
-    let routes = routes![index,
-        get,
+    let routes = routes![
+        index,
+        random,
+        question::read,
+        question::create,
+        question::update,
+        question::delete,
+        tag::read,
+        tag::create,
+        tag::update,
+        tag::delete
     ];
     rocket::ignite().mount("/", routes).launch();
 }
