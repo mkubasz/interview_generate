a996eec0a2368d5626483d9e34ac83350be29e1e
mkubasz <mkubasz@gmail.com>
2019-04-05 11:49:50 +0200

    Add base structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+use rocket::response::content::Json;
+use std::collections;
+use std::hash::Hash;
+
+enum Tag {
+    OOP
+}
+
+enum Level {
+    JUNIOR,
+    MEDIUM,
+    SENIOR
+}
+
+struct Question {
+    id: Hash,
+    question: String,
+    answer: String,
+    level: Level,
+    tags: Vec<Tag>
+}
+
+struct Filter {
+    level: Level,
+    tags: Vec<Tag>
+}
+
+trait QuestionsRepo {
+    fn create(question: Question);
+    fn get(filter: Filter);
+    fn update(question: Question);
+    fn delete(id: Box<Hash>);
+}
+
+impl QuestionRepo {
+    // TODO
+}
+
+#[get("/")]
+fn index() -> Json<&'static str> {
+    Json("{'routes' : ['/get/<topic>/<level>/<tag>']}")
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![index]).launch();
 }
